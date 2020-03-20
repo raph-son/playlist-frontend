47590c93538611281740b04147d9088ef2b02d44
raph-son <samezzy@yahoo.com>
2020-03-20 10:26:03 +0100

    Mobile support adjustment

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,25 @@ window.addEventListener("DOMContentLoaded", function(event) {
     var modal_flex = document.querySelector("#modal_flex");
 
     create_button.addEventListener("click", function() {
-        if (modal_flex.style.visibility === "visible") {
-            modal_flex.style.visibility = "hidden";
-        }
-        else {
-            modal_flex.style.visibility = "visible";
-        }
+        toggle_visibility(modal_flex);
     })
     
     document.querySelector("#modal_buttons button:nth-child(1)").addEventListener("click", function() {
         document.querySelector("#modal_input_text > input").value = "";
         modal_flex.style.visibility = "hidden";
     })
+
+    document.querySelector("#header_navs_mobile ").addEventListener("click", function() {
+        let drop_down = document.querySelector("#header_navs_mobile > ul");
+        toggle_visibility(drop_down)
+    })
 });
+
+function toggle_visibility(object) {
+    if (object.style.visibility === "visible") {
+        object.style.visibility = "hidden";
+    }
+    else {
+        object.style.visibility = "visible";
+    }
+}
